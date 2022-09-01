8810e36950ed9c2e9f6fe36e8d6802cdbd1bb435
Bikrant27 <maharjanbikrant9@gmail.com>
2022-09-01 11:16:56 +0545

    Todo app improved

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Todo.css'
 
 const Todo = () => {
+    const [inputData, setInputData] = useState('');
+    const [todos, setTodos] = useState([
+        'Walk for fresh.',
+        'Rest a while.',
+        'Feed a dog.',
+        'Watch a movie.'
+    ]);
+
+    function handleSubmit(){
+        setTodos([...todos, inputData])
+        setInputData('')
+    }
+
+    function deleteAll(){
+        setTodos([''])
+    }
+
+    function Edit(){
+
+    }
+
+    function Delete(){
+    
+    }
+
   return (
         <div class="container">
             <h3 class="logo">Todo App</h3>
-            <input type="text" class ="input" placeholder='Enter the Items'></input>
-            <button class="button">Add Todo</button>
+            <input 
+                type="text" 
+                class ="input" 
+                placeholder='Enter the Items' 
+                value={inputData}
+                onChange = {(e) => setInputData(e.target.value)}
+                >
+            </input>
+            <button 
+                class="button"
+                onClick = {handleSubmit}>Add Todo</button>
             <h3 class="todo-list">To Do Lists</h3>
             <ul class="list">
-                <li>Hello 1</li>
-                <li>Task 2</li>
-                <li>Task 3</li>
+                {todos.map((todo) => (
+                <li>{todo}
+                <button onClick={Edit}>Edit</button>
+                <button onClick={Delete}>Delete</button>
+                </li> ))}
             </ul>
-            <button class="remove">Remove All</button>
+            <button
+                class="remove"
+                onClick= {deleteAll}>Remove All</button>
         </div>
     )
 }
